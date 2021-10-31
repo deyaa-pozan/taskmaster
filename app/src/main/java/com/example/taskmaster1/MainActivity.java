0b83a9303d5645d5af705a8e22da1e78e789ab69
deyaa-pozan <deyaa.pozan@gmail.com>
2021-10-31 22:33:23 +0200

    lab26

--- app/src/main/java/com/example/taskmaster1/MainActivity.java
+++ app/src/main/java/com/example/taskmaster1/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(goToAllTasks);
             }
         });
+
 
     }
 }

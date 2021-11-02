9ea5f6e646ec427600f49292179054f4bc31fcac
deyaa-pozan <deyaa.pozan@gmail.com>
2021-11-02 23:33:13 +0200

    lab 28

--- app/src/main/java/com/example/taskmaster1/MainActivity.java
+++ app/src/main/java/com/example/taskmaster1/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.taskmaster1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -9,6 +10,10 @@ import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import java.util.ArrayList;
+import java.util.List;
+
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,10 +24,31 @@ public class MainActivity extends AppCompatActivity {
 
         Button addTask = findViewById(R.id.button_first);
         Button allTasks = findViewById(R.id.button_first2);
-        Button task1 = findViewById(R.id.task1);
-        Button task2 = findViewById(R.id.task2);
-        Button task3 = findViewById(R.id.task3);
+//        Button task1 = findViewById(R.id.task1);
+//        Button task2 = findViewById(R.id.task2);
+//        Button task3 = findViewById(R.id.task3);
         Button settings = findViewById(R.id.settings);
+        String body = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras felis massa, elementum a nibh sed, sodales posuere nunc. Vivamus eget ante malesuada, fermentum tellus eget, dignissim enim. Duis felis enim, facilisis in tortor eget, pellentesque tristique dolor. Nullam hendrerit ex at sagittis tincidunt. Cras in sodales mauris. Quisque lobortis nisl quis rhoncus accumsan. ";
+        RecyclerView recyclerView = findViewById(R.id.RV_main);
+        List tasks = new ArrayList<>();
+        Task t1 = new Task("Clean the room", body, State.NEW );
+        Task t2 = new Task("Study 2 hours", body, State.NEW );
+        Task t3 = new Task("Eat your meal", body, State.NEW );
+        Task t4 = new Task("Take a shower", body, State.NEW );
+        Task t5 = new Task("Feed yor cat", body, State.NEW );
+        Task t6 = new Task("Sleep 6 hours", body, State.NEW );
+        tasks.add(t1);
+        tasks.add(t2);
+        tasks.add(t3);
+        tasks.add(t4);
+        tasks.add(t5);
+        tasks.add(t6);
+        TaskAdapter taskAdapter = new TaskAdapter(tasks, this);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.canScrollVertically();
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setAdapter(taskAdapter);
 
         addTask.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,35 +66,35 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        task1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent goToTaskOne = new Intent(MainActivity.this, OneTaskActivity.class);
-                String task = task1.getText().toString();
-                goToTaskOne.putExtra("task",task);
-                startActivity(goToTaskOne);
-            }
-        });
-
-        task2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent goToTaskTwo = new Intent(MainActivity.this, OneTaskActivity.class);
-                String task = task2.getText().toString();
-                goToTaskTwo.putExtra("task",task);
-                startActivity(goToTaskTwo);
-            }
-        });
-
-        task3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String task = task3.getText().toString();
-                Intent goToTaskThree = new Intent(MainActivity.this, OneTaskActivity.class);
-                goToTaskThree.putExtra("task",task);
-                startActivity(goToTaskThree);
-            }
-        });
+//        task1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent goToTaskOne = new Intent(MainActivity.this, OneTaskActivity.class);
+//                String task = task1.getText().toString();
+//                goToTaskOne.putExtra("task",task);
+//                startActivity(goToTaskOne);
+//            }
+//        });
+//
+//        task2.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent goToTaskTwo = new Intent(MainActivity.this, OneTaskActivity.class);
+//                String task = task2.getText().toString();
+//                goToTaskTwo.putExtra("task",task);
+//                startActivity(goToTaskTwo);
+//            }
+//        });
+//
+//        task3.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                String task = task3.getText().toString();
+//                Intent goToTaskThree = new Intent(MainActivity.this, OneTaskActivity.class);
+//                goToTaskThree.putExtra("task",task);
+//                startActivity(goToTaskThree);
+//            }
+//        });
 
         settings.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -77,7 +103,6 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(goToSettings);
             }
         });
-
     }
 
     @Override
---
+++ app/src/main/java/com/example/taskmaster1/TaskAdapter.java
@@ -0,0 +1,71 @@
+package com.example.taskmaster1;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.TaskViewHolder> {
+
+    List<Task> tasks = new ArrayList<>();
+    Context context;
+
+    public TaskAdapter(List<Task> tasks, Context context) {
+        this.context = context;
+        this.tasks = tasks;
+    }
+
+    public static class TaskViewHolder extends RecyclerView.ViewHolder{
+
+
+        TextView titleT, bodyT, stateT;
+        View itemView;
+        public TaskViewHolder(@NonNull View itemView) {
+            super(itemView);
+            this.itemView = itemView;
+            titleT = itemView.findViewById(R.id.fragment_title_text);
+            bodyT = itemView.findViewById(R.id.fragment_body_text);
+            stateT = itemView.findViewById(R.id.fragment_state_text);
+        }
+    }
+
+
+    @NonNull
+    @Override
+    public TaskAdapter.TaskViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.fragment_task,parent,false);
+
+        return new TaskViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TaskAdapter.TaskViewHolder holder, int position) {
+        Task task = tasks.get(position);
+        holder.titleT.setText(task.title);
+        holder.bodyT.setText(task.body);
+        holder.stateT.setText(task.state.toString());
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent taskDetailsIntent = new Intent(context, OneTaskActivity.class);
+                taskDetailsIntent.putExtra("title",task.title);
+                context.startActivity(taskDetailsIntent);
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return tasks.size();
+    }
+}
